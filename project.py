2cf05e58f3de37e46ecaddb68abb19d2ad02b2ae
Anish Veereddy <akv5491@psu.edu>
2026-04-28 21:31:32 -0400

    updated game

---
+++ project.py
@@ -0,0 +1,27 @@
+import random
+
+number = random.randint(1, 100)
+
+attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("Guess a number between 1 and 100.")
+
+while True:
+    guess = input("Enter your guess: ")
+
+    if not guess.isdigit():
+        print("Please enter a valid number.")
+        continue
+
+    guess = int(guess)
+    attempts += 1
+
+    if guess > number:
+        print("Too high! Try a lower number.")
+    elif guess < number:
+        print("Too low! Try a higher number.")
+    else:
+        print("Congratulations! You guessed the number!")
+        print("Number of attempts:", attempts)
+        break
